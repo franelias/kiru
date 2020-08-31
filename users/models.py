b028a9b52bf056acbf0f88eaf714cd64c2b77ed2
Francisco Elias <fielias81@gmail.com>
2020-08-30 23:32:07 -0300

    Added Users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.TextField()
+    password = models.TextField()
+    email = models.TextField()
+    type = models.BooleanField()
