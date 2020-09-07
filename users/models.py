a5aa1f09b926cd99dcfd7ff806e98da4e5534bc3
Francisco Elias <fielias81@gmail.com>
2020-09-06 22:25:24 -0300

    *Added Django Rest Framework *Eliminated 'Users' model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
-    username = models.TextField()
-    password = models.TextField()
-    email = models.TextField()
-    type = models.BooleanField()
