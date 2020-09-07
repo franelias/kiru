d7d9b03498746170a91e350b3e132de24d578150
Francisco Elias <fielias81@gmail.com>
2020-09-06 23:09:15 -0300

    Added jwt module

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from users.serializers import UserSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+@csrf_exempt
+def list_users(request):
+    permission_classes = (IsAuthenticated,)
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse(serializer.data, status=200, safe=False)
+
+    return JsonResponse({'message':'Method Not Allowed'}, status=405)
