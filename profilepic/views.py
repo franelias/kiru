a4e7efc84455e739683b62506a210640ecff3fd6
Francisco Elias <fielias81@gmail.com>
2020-10-13 00:01:49 -0300

    Removed some imports from profilepic's views.py

--- profilepic/views.py
+++ profilepic/views.py
@@ -1,7 +1,4 @@
 from rest_framework.parsers import FileUploadParser
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework import viewsets
 from profilepic.serializers import FileSerializer
 from rest_framework.permissions import IsAuthenticated
