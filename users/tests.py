0c3d645ffa1c15b0780ea6a8fa31b6db08dbc4c4
Francisco Elias <fielias81@gmail.com>
2020-10-13 00:37:48 -0300

    Added jwt authentication test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='test',password='test')
+        user.save()
+
+    def test_jwt_authentication(self):
+        response = self.client.post('/api/token/',{'username':'test','password':'test'})
+        self.assertEqual(response.status_code, 200)
